086e06a7d472974a58c01581dd2faa226a1b1f5e
anigandini <anigandini@gmail.com>
2022-06-20 16:34:25 -0300

    Item type

---
+++ client/src/interfaces.ts
@@ -0,0 +1,20 @@
+export interface Author {
+    "name": string,
+    "lastname": string
+}
+
+export interface Item {
+    "id": string,
+    "title": string,
+    "price": {
+        "currency": string,
+        "amount": number,
+        "decimals": number
+        
+    },
+    "picture": string,
+    "condition": string,
+    "free_shipping": boolean,
+    "sold_quantity"?: number,
+    "description"?: string
+}
