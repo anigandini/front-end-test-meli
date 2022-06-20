ce3ed3ddd0ca8124bee93e4f49eb5921e46a8f69
anigandini <anigandini@gmail.com>
2022-06-20 16:30:12 -0300

    app root component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import "./styles/sass/main.sass"
-import SearchBar from './components/SearchBar'
-import SearchResult from './components/searchResult'
-import Icon from './components/Icon'
+import React , { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "./styles/sass/main.sass"
+import SearchBar from './components/UI/SearchBar'
+import Loader from './components/UI/Loader'
+const BreadCrumbs = lazy(() => import('./components/UI/BreadCrumbs'))
+const Product = lazy(() => import('./components/pages/Product'))
+const SearchResult = lazy(() => import('./components/pages/searchResult'))
 
 function App() {
   const [ searchResult, setSearchResult ] = React.useState([])
   const [ searchQuery, setSearchQuery ] = React.useState('')
-
-
+  const [ categories, setCategories ] = React.useState(null)
+  
   return (
     <Router>
       <div className="app">
         <header className="header">
-          <SearchBar setSearchResult={setSearchResult} searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+          <SearchBar setSearchResult={setSearchResult} searchQuery={searchQuery} setSearchQuery={setSearchQuery} setCategories={setCategories} />
         </header>
         <main>
+          {categories !== null && <BreadCrumbs categories={categories}/>}
+          <Suspense fallback={<Loader/>}>
             <Routes>
               <Route path='/' />
-              <Route path='/items/:id' />
-              <Route path={`/items/search/:q`} element={<SearchResult items={searchResult}/>}/>
+              <Route path={`/items/:id`} element={<Product setCategories={setCategories}/>}/>
+              <Route path={`/items/search/:q`} element={<SearchResult items={searchResult} setItems={setSearchResult} setCategories={setCategories}/>}/>
             </Routes>
-          </main>
+          </Suspense>
+        </main>
       </div>
     </Router>
   )
